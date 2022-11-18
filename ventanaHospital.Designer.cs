1e2bf92c50a32322c3e7a26f66b75db04ed06554
29Jeanca <105949714+29Jeanca@users.noreply.github.com>
2022-11-18 17:24:34 -0600

    prueba

--- ventanaHospital.Designer.cs
+++ ventanaHospital.Designer.cs
@@ -28,22 +28,323 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtIdHospital = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAreaTerreno = new System.Windows.Forms.TextBox();
+            this.txtValorFiscal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtProvincia = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCanton = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtDistrito = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtCantNiveles = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtCantConsultorios = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtTiposCirugia = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtAnoCostruccion = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // txtIdHospital
+            // 
+            this.txtIdHospital.Location = new System.Drawing.Point(61, 25);
+            this.txtIdHospital.Name = "txtIdHospital";
+            this.txtIdHospital.Size = new System.Drawing.Size(100, 20);
+            this.txtIdHospital.TabIndex = 0;
+            this.txtIdHospital.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // txtAreaTerreno
+            // 
+            this.txtAreaTerreno.Location = new System.Drawing.Point(61, 103);
+            this.txtAreaTerreno.Name = "txtAreaTerreno";
+            this.txtAreaTerreno.Size = new System.Drawing.Size(100, 20);
+            this.txtAreaTerreno.TabIndex = 2;
+            this.txtAreaTerreno.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtValorFiscal
+            // 
+            this.txtValorFiscal.Location = new System.Drawing.Point(61, 64);
+            this.txtValorFiscal.Name = "txtValorFiscal";
+            this.txtValorFiscal.Size = new System.Drawing.Size(100, 20);
+            this.txtValorFiscal.TabIndex = 3;
+            this.txtValorFiscal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Valor Fiscal";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Area terreno";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Dirección exacta";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(61, 142);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(100, 20);
+            this.txtDireccion.TabIndex = 7;
+            this.txtDireccion.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(58, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Provincia";
+            // 
+            // txtProvincia
+            // 
+            this.txtProvincia.Location = new System.Drawing.Point(61, 181);
+            this.txtProvincia.Name = "txtProvincia";
+            this.txtProvincia.Size = new System.Drawing.Size(100, 20);
+            this.txtProvincia.TabIndex = 9;
+            this.txtProvincia.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(58, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Cantón";
+            // 
+            // txtCanton
+            // 
+            this.txtCanton.Location = new System.Drawing.Point(61, 220);
+            this.txtCanton.Name = "txtCanton";
+            this.txtCanton.Size = new System.Drawing.Size(100, 20);
+            this.txtCanton.TabIndex = 11;
+            this.txtCanton.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(58, 243);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Distrito";
+            // 
+            // txtDistrito
+            // 
+            this.txtDistrito.Location = new System.Drawing.Point(61, 259);
+            this.txtDistrito.Name = "txtDistrito";
+            this.txtDistrito.Size = new System.Drawing.Size(100, 20);
+            this.txtDistrito.TabIndex = 13;
+            this.txtDistrito.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(58, 282);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(102, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Cantidad de Niveles";
+            // 
+            // txtCantNiveles
+            // 
+            this.txtCantNiveles.Location = new System.Drawing.Point(60, 298);
+            this.txtCantNiveles.Name = "txtCantNiveles";
+            this.txtCantNiveles.Size = new System.Drawing.Size(101, 20);
+            this.txtCantNiveles.TabIndex = 15;
+            this.txtCantNiveles.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(58, 321);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Color";
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(60, 337);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(100, 20);
+            this.txtColor.TabIndex = 17;
+            this.txtColor.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(57, 360);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(124, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Cantidad de Consultorios";
+            // 
+            // txtCantConsultorios
+            // 
+            this.txtCantConsultorios.Location = new System.Drawing.Point(58, 376);
+            this.txtCantConsultorios.Name = "txtCantConsultorios";
+            this.txtCantConsultorios.Size = new System.Drawing.Size(103, 20);
+            this.txtCantConsultorios.TabIndex = 19;
+            this.txtCantConsultorios.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(55, 399);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(85, 13);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Tipos de Cirugía";
+            // 
+            // txtTiposCirugia
+            // 
+            this.txtTiposCirugia.Location = new System.Drawing.Point(57, 415);
+            this.txtTiposCirugia.Name = "txtTiposCirugia";
+            this.txtTiposCirugia.Size = new System.Drawing.Size(103, 20);
+            this.txtTiposCirugia.TabIndex = 21;
+            this.txtTiposCirugia.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(55, 438);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(90, 13);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Año construcción";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(57, 454);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(103, 20);
+            this.dateTimePicker1.TabIndex = 23;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(81, 477);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(42, 13);
+            this.label13.TabIndex = 24;
+            this.label13.Text = "Imagen";
+            // 
+            // txtAnoCostruccion
+            // 
+            this.txtAnoCostruccion.Location = new System.Drawing.Point(57, 493);
+            this.txtAnoCostruccion.Name = "txtAnoCostruccion";
+            this.txtAnoCostruccion.Size = new System.Drawing.Size(103, 20);
+            this.txtAnoCostruccion.TabIndex = 25;
             // 
             // ventanaHospital
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(601, 519);
+            this.Controls.Add(this.txtAnoCostruccion);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtTiposCirugia);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtCantConsultorios);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtCantNiveles);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtDistrito);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtCanton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtProvincia);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtValorFiscal);
+            this.Controls.Add(this.txtAreaTerreno);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIdHospital);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ventanaHospital";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registro de Hospitales";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtIdHospital;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAreaTerreno;
+        private System.Windows.Forms.TextBox txtValorFiscal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtProvincia;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCanton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDistrito;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtCantNiveles;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtCantConsultorios;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtTiposCirugia;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtAnoCostruccion;
     }
 }
 
